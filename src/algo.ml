fb21c904bdc788b2e045b25dd295d2f9a7031e60
YacineSteeve <steeveboukari9@gmail.com>
2025-01-09 05:01:10 +0100

    docs: more comments

--- src/algo.ml
+++ src/algo.ml
@@ -3,8 +3,12 @@ open Tools
 
 type path = id list
 
+(* Arcs with label 0 are ignored for this specific Ford-Fulkerson implementation
+ * (Here "no arc" then means "no arc or no arc with non null label")
+ *)
 let children graph node = List.fold_left (fun l arc -> if arc.lbl != 0 then arc.tgt :: l else l) [] (out_arcs graph node)
 
+(* BFS *)
 let rec find_path graph forbidden src tgt =
   if src = tgt
   then Some [src]
@@ -19,8 +23,10 @@ let rec find_path graph forbidden src tgt =
     loop_children (List.filter (fun e -> not (List.exists (fun f -> f = e) forbidden)) (children graph src))
   )
 
+(* Create a copy of the graph with every labels at 0 *)
 let init_flow_graph graph = e_fold graph (fun g arc -> add_arc g arc.src arc.tgt 0) (clone_nodes graph)
 
+(* Get the minimum flow along a path, which corresponds to the flow increment *)
 let rec get_flow_increment graph = function
   | []  | [_] -> max_int
   | src :: tgt :: rest -> (
@@ -29,6 +35,7 @@ let rec get_flow_increment graph = function
       | Some arc -> min arc.lbl (get_flow_increment graph (tgt :: rest))
     )
 
+(* Add the flow increment along a path *)
 let rec update_flow_graph graph increment = function
   | []  | [_] -> graph
   | src :: tgt :: rest -> update_flow_graph (add_arc graph src tgt increment) increment (tgt :: rest)
@@ -36,8 +43,10 @@ let rec update_flow_graph graph increment = function
 let rec update_residual_graph graph increment = function
   | []  | [_] -> graph
   | src :: tgt :: rest ->
+    (* Subtract the increment for the normal arc direction, and add it for the inverse arc direction *)
     update_residual_graph (add_arc (add_arc graph src tgt (-increment)) tgt src increment) increment (tgt :: rest)
 
+(* Basically removes non relevant arcs *)
 let get_final_graph initial_graph residual_graph =
   e_fold initial_graph (
     fun g arc -> match find_arc residual_graph arc.tgt arc.src with
